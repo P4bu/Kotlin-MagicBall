f1344b105a9825894f03dea40f3a7eb397ef7030
Pablo Barra urtubia <pbarrau.90@icloud.com>
2024-05-10 11:56:22 -0400

    Creacion de juego MagicBall con Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,81 @@
+// Declaramos los valores de nuestras respuestas
+const val RESPUESTA_AFIRMATIVA = "✅"
+const val RESPUESTA_NEGATIVA = "❌"
+const val RESPUESTA_DUDOSA = "?"
+
+//Unimos las respuestas con los valores
+val respuestas = mapOf(
+    "Sí" to RESPUESTA_AFIRMATIVA,
+    "Es cierto" to RESPUESTA_AFIRMATIVA,
+    "Totalmente" to RESPUESTA_AFIRMATIVA,
+    "Sin duda" to RESPUESTA_AFIRMATIVA,
+    "Pregunta en otro momento" to RESPUESTA_DUDOSA,
+    "No puedo decirte en este momento" to RESPUESTA_DUDOSA,
+    "Puede que si o puede que no" to RESPUESTA_DUDOSA,
+    "No va a suceder" to RESPUESTA_NEGATIVA,
+    "No cuentes con ello" to RESPUESTA_NEGATIVA,
+    "Definitivamente no" to RESPUESTA_NEGATIVA,
+    "No lo creo" to RESPUESTA_NEGATIVA,
+)
+
+fun main() {
+    println("Hola, soy tu bola 8 magica, Cual de estas opciones deseas realizar")
+    println("1. Hacer una pregunta")
+    println("2. Revisar todas las respuestas")
+    println("3. Salir")
+
+    val opcion = readLine()
+    when(opcion) {
+        "1" -> realizarPregunta()
+        "2" -> mostrarRespuestas()
+        "3" -> salir()
+        else -> mostrarError()
+    }
+}
+
+fun mostrarError() {
+    println("ELEGISTE UNA RESPUESTA NO VALIDA, HASTA PRONTO")
+}
+
+fun salir() {
+    println("HASTA PRONTO")
+}
+
+fun mostrarRespuestas() {
+    println("Selecciona una opcion")
+    println("1. Revisar todas las respuestas")
+    println("2. Revisar solo las afirmativas")
+    println("3. Revisar solo las negativas")
+    println("4. Revisar solo las dudosas")
+    val opcionIngresada = readLine()
+    when(opcionIngresada){
+        "1" -> mostrarRespuestasTipo()
+        "2" -> mostrarRespuestasTipo(tipoRespuesta = RESPUESTA_AFIRMATIVA)
+        "3" -> mostrarRespuestasTipo(tipoRespuesta = RESPUESTA_NEGATIVA)
+        "4" -> mostrarRespuestasTipo(tipoRespuesta = RESPUESTA_DUDOSA)
+        else -> println("Respuesta no valida")
+    }
+}
+
+fun mostrarRespuestasTipo(tipoRespuesta : String = "TODOS"){
+    when(tipoRespuesta){
+        "TODOS" -> respuestas.keys.forEach { respuesta -> println(respuesta)}
+
+        RESPUESTA_AFIRMATIVA -> respuestas.filterValues { valores -> valores == RESPUESTA_AFIRMATIVA }
+            .also { respuestasPositivas -> println(respuestasPositivas.keys)}
+
+        RESPUESTA_NEGATIVA -> respuestas.filterValues { valores -> valores == RESPUESTA_NEGATIVA }
+            .also { respuestasNegativas -> println(respuestasNegativas.keys)}
+
+        RESPUESTA_DUDOSA -> respuestas.filterValues { valores -> valores == RESPUESTA_DUDOSA }
+            .also { respuestasDudosas -> println(respuestasDudosas.keys)}
+    }
+}
+
+fun realizarPregunta(){
+    println("Que pregunta deseas realizar?")
+    readLine()
+    println("Asi que esa era tu pregunta... la respusta a eso es -> ")
+    val respuestaGenerada = respuestas.keys.random();
+    println(respuestaGenerada)
+}
